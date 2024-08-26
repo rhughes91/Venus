d7c793812136b13bc48a7af179068d3bcf164670
Ryan Hughes <rohughes91@gmail.com>
2024-08-25 22:56:48 -0500

    Linux support.

--- build-tools/main.cpp
+++ build-tools/main.cpp
@@ -1,7 +1,10 @@
 #include <algorithm>
 #include <filesystem>
 #include <iostream>
-#include <windows.h>
+
+#if defined(_WIN32)
+  #include <windows.h>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -26,6 +29,10 @@ int main(int argc, char *argv[])
     std::filesystem::remove_all(source + "/../../Builds/"+argv[1]);
     std::filesystem::create_directory(source + "/../../Builds/"+argv[1]);
     std::filesystem::copy(source + "/../projects/"+argv[1]+"/resources", source + "/../../Builds/"+argv[1]+"/resources", std::filesystem::copy_options::skip_existing | std::filesystem::copy_options::recursive);
+    #if defined(_WIN32)
     std::filesystem::copy(source + "/../build/"+argv[1]+"/"+argv[1]+".exe", source + "/../../Builds/"+argv[1]+"/"+argv[1]+".exe", std::filesystem::copy_options::skip_existing | std::filesystem::copy_options::recursive);
+    #elif defined(__linux__)
+    std::filesystem::copy(source + "/../build/"+argv[1]+"/"+argv[1], source + "/../../Builds/"+argv[1]+"/"+argv[1], std::filesystem::copy_options::skip_existing | std::filesystem::copy_options::recursive);
+    #endif
     std::filesystem::copy(source + "/path.config", source + "/../../Builds/"+argv[1]+"/path.config", std::filesystem::copy_options::skip_existing | std::filesystem::copy_options::recursive);
 }
